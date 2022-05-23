0e37aa53751f11e9063346fc6d1a229dd68c2336
OPBrother <2276720277@qq.com>
2022-05-23 09:53:41 +0800

    update smach

--- state_machine.py
+++ state_machine.py
@@ -9,157 +9,169 @@ import smach
 import smach_ros
 from std_msgs.msg import String
 from status_handler import status_handler    
-from aliengo_state_mach.msg import RoverStateMsg,RoverActionMsg
+from turtlebot3_msgs.msg import RoverStateMsg,RoverActionMsg
  
 status_handler = status_handler()
 actionMsg = RoverActionMsg()
 
  
         
-class IDLE_IDLE_1(smach.State):
+class S0(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['first_goal_started_1','repeat'])
+    smach.State.__init__(self, outcomes=['first','run_into_wall','repeat'])
     self.stateMsg = RoverStateMsg()
 
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.IDLE_IDLE_1      
+    self.stateMsg.state = self.stateMsg.S0      
     status_handler.publishRoverState(self.stateMsg)
-    if(status_handler.action.ACTION  ==  actionMsg.FIRST_GOAL_STARTED):      
-      return 'first_goal_started_1'
+    if(status_handler.action.ACTION  ==  actionMsg.FIRST):      
+      return 'first'
+    if(status_handler.action.ACTION  ==  actionMsg.RUN_INTO_WALL):      
+      return 'run_into_wall'
     return 'repeat'
 
-class WALKING_IDLE_1(smach.State):
+class S1(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['first_goal_finished_3','robot_falls_0','repeat'])
+    smach.State.__init__(self, outcomes=['second','run_into_wall','repeat'])
     self.stateMsg = RoverStateMsg()
 
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.WALKING_IDLE_1      
+    self.stateMsg.state = self.stateMsg.S1      
     status_handler.publishRoverState(self.stateMsg)  
-    if(status_handler.action.ACTION ==  actionMsg.FIRST_GOAL_FINISHED):    
-      return 'first_goal_finished_3'
-    if (status_handler.action.ACTION ==  actionMsg.ROBOT_FALLS):    
-      return 'robot_falls_0'
+    if(status_handler.action.ACTION ==  actionMsg.SECOND):    
+      return 'second'
+    if (status_handler.action.ACTION ==  actionMsg.RUN_INTO_WALL):    
+      return 'run_into_wall'
     return 'repeat'
 
-class DOCKING_IDLE(smach.State):
+class S2(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['docking_finished_5','robot_falls_0','repeat'])
+    smach.State.__init__(self, outcomes=['third','run_into_wall','repeat'])
     self.stateMsg = RoverStateMsg()
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.DOCKING_IDLE      
+    self.stateMsg.state = self.stateMsg.S2      
     status_handler.publishRoverState(self.stateMsg)  
-    if(status_handler.action.ACTION ==  actionMsg.DOCKING_FINISHED):    
-      return 'docking_finished_5'
-    if (status_handler.action.ACTION ==  actionMsg.ROBOT_FALLS):    
-      return 'robot_falls_0'
+    if(status_handler.action.ACTION ==  actionMsg.THIRD):    
+      return 'third'
+    if (status_handler.action.ACTION ==  actionMsg.RUN_INTO_WALL):    
+      return 'run_into_wall'
     return 'repeat'
 
-class IDLE_IDLE_2(smach.State):
+class S3(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['moving_box_started_19','repeat'])
+    smach.State.__init__(self, outcomes=['forth','collision2','repeat'])
     self.stateMsg = RoverStateMsg()
 
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.IDLE_IDLE_2      
+    self.stateMsg.state = self.stateMsg.S3      
     status_handler.publishRoverState(self.stateMsg)
-    if(status_handler.action.ACTION ==  actionMsg.MOVING_BOX_STARTED):      
-      return 'moving_box_started_19'
+    if(status_handler.action.ACTION ==  actionMsg.FORTH):      
+      return 'forth'
+    if(status_handler.action.ACTION ==  actionMsg.COLLISION2):      
+      return 'collision2'
     return 'repeat'
 
-class IDLE_WORKING(smach.State):
+class S4(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['moving_box_ended_7','box_falls_2','repeat'])
+    smach.State.__init__(self, outcomes=['fifth','collision2','repeat'])
     self.stateMsg = RoverStateMsg()
 
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.IDLE_WORKING      
+    self.stateMsg.state = self.stateMsg.S4      
     status_handler.publishRoverState(self.stateMsg)  
-    if(status_handler.action.ACTION ==  actionMsg.MOVING_BOX_ENDED):    
-      return 'moving_box_ended_7'
-    if (status_handler.action.ACTION ==  actionMsg.BOX_FALLS):    
-      return 'box_falls_2'
+    if(status_handler.action.ACTION ==  actionMsg.FIFTH):    
+      return 'fifth'
+    if (status_handler.action.ACTION ==  actionMsg.COLLISION2):    
+      return 'collision2'
     return 'repeat'
 
-class IDLE_IDLE_3(smach.State):
+class S5(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['box_is_on_the_robot_21','repeat'])
+    smach.State.__init__(self, outcomes=['sixth','collision3','repeat'])
     self.stateMsg = RoverStateMsg()
 
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.IDLE_IDLE_3      
+    self.stateMsg.state = self.stateMsg.S5      
     status_handler.publishRoverState(self.stateMsg)
-    if(status_handler.action.ACTION ==  actionMsg.BOX_IS_ON_THE_ROBOT):      
-      return 'box_is_on_the_robot_21'
+    if(status_handler.action.ACTION ==  actionMsg.SIXTH):      
+      return 'sixth'
+    if (status_handler.action.ACTION ==  actionMsg.COLLISION3):    
+      return 'collison3'
     return 'repeat'
 
-class STANDING_IDLE(smach.State):
+class S6(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['second_goal_started_9','robot_falls_4','repeat'])
+    smach.State.__init__(self, outcomes=['seventh','collision3','repeat'])
     self.stateMsg = RoverStateMsg()
 
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.STANDING_IDLE      
+    self.stateMsg.state = self.stateMsg.S6      
     status_handler.publishRoverState(self.stateMsg)  
-    if(status_handler.action.ACTION ==  actionMsg.SECOND_GOAL_STARTED):    
-      return 'second_goal_started_9'
-    if (status_handler.action.ACTION  ==  actionMsg.ROBOT_FALLS):    
-      return 'robot_falls_4'
+    if(status_handler.action.ACTION ==  actionMsg.SEVENTH):    
+      return 'seventh'
+    if (status_handler.action.ACTION  ==  actionMsg.COLLISION3):    
+      return 'collision3'
     return 'repeat'
 
-class WALKING_IDLE_2(smach.State):
+class S7(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['second_goal_finished_11','robot_falls_4','repeat'])
+    smach.State.__init__(self, outcomes=['eighth','collision4','repeat'])
     self.actionMsg = status_handler.action
     self.stateMsg = RoverStateMsg()
 
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.WALKING_IDLE_2      
+    self.stateMsg.state = self.stateMsg.S7      
     status_handler.publishRoverState(self.stateMsg)  
-    if(status_handler.action.ACTION  ==  self.actionMsg.SECOND_GOAL_FINISHED):    
-      return 'second_goal_finished_11'
-    if (status_handler.action.ACTION  ==  self.actionMsg.ROBOT_FALLS):    
-      return 'robot_falls_4'
+    if(status_handler.action.ACTION  ==  self.actionMsg.EIGHTH):    
+      return 'eighth'
+    if (status_handler.action.ACTION  ==  self.actionMsg.COLLISION4):    
+      return 'collision4'
     return 'repeat'
 
-class ROBOT_FAIL_1(smach.State):
+class FALL1(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['error_flag_17'])
+    smach.State.__init__(self, outcomes=['reset'])
     self.stateMsg = RoverStateMsg()
 
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.ROBOT_FAIL_1      
+    self.stateMsg.state = self.stateMsg.FALL1      
     status_handler.publishRoverState(self.stateMsg)
-    return 'error_flag_17'
+    if (status_handler.action.ACTION  ==  self.actionMsg.RESET):    
+      return 'reset'
 
-class ROBOT_FAIL_2(smach.State):
+class FALL2(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['error_flag_23'])
+    smach.State.__init__(self, outcomes=['reset2'])
     self.stateMsg = RoverStateMsg()
 
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.ROBOT_FAIL_2      
+    self.stateMsg.state = self.stateMsg.FALL2      
     status_handler.publishRoverState(self.stateMsg)
-    return 'error_flag_23'
+    if (status_handler.action.ACTION  ==  self.actionMsg.RESET2):    
+      return 'reset2'
 
-class BOX_FAIL(smach.State):
+class FALL3(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['spawn_box_15'])
+    smach.State.__init__(self, outcomes=['reset3'])
     self.stateMsg = RoverStateMsg()
 
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.BOX_FAIL      
+    self.stateMsg.state = self.stateMsg.FALL3      
     status_handler.publishRoverState(self.stateMsg)
-    return 'spawn_box_15'
-class SUCCES_IDLE(smach.State):
+    if (status_handler.action.ACTION  ==  self.actionMsg.RESET3):    
+      return 'reset3'
+
+class FALL4(smach.State):
   def __init__(self):
-    smach.State.__init__(self, outcomes=['succes_flag_13'])
+    smach.State.__init__(self, outcomes=['reset4'])
     self.stateMsg = RoverStateMsg()
 
   def execute(self, userdata):
-    self.stateMsg.state = self.stateMsg.BOX_FAIL      
+    self.stateMsg.state = self.stateMsg.FALL4     
     status_handler.publishRoverState(self.stateMsg)
-    return 'succes_flag_13'
+    if (status_handler.action.ACTION  ==  self.actionMsg.RESET4):    
+      return 'reset4'
+
 #####################################################################################################################################################
         
 
@@ -172,39 +184,39 @@ def CreateStateMachine():
     #Open the container
     with sm_rover:
 
-        smach.StateMachine.add('IDLE_IDLE_1', IDLE_IDLE_1(),
-                               transitions={'first_goal_started_1': 'WALKING_IDLE_1','repeat':'IDLE_IDLE_1', })        
-        smach.StateMachine.add('ROBOT_FAIL_1', ROBOT_FAIL_1(), 
-                               transitions={'error_flag_17':'IDLE_IDLE_1' })
+        smach.StateMachine.add('S0', S0(),
+                               transitions={'first': 'S1','run_into_wall':'RUN_INTO_WALL','repeat':'S0', })        
+        smach.StateMachine.add('RUN_INTO_WALL', FALL1(), 
+                               transitions={'reset':'S0' })
 
-        smach.StateMachine.add('ROBOT_FAIL_2', ROBOT_FAIL_2(), 
-                               transitions={'error_flag_23':'IDLE_IDLE_1' })
-        smach.StateMachine.add('BOX_FAIL', BOX_FAIL(), 
-                               transitions={'spawn_box_15':'IDLE_IDLE_1' })
+        smach.StateMachine.add('COLLISION2', FALL2(), 
+                               transitions={'reset2':'S3' })
+        smach.StateMachine.add('COLLISION3', FALL3(), 
+                               transitions={'reset3':'S5' })
 
-        smach.StateMachine.add('WALKING_IDLE_1', WALKING_IDLE_1(), 
-                               transitions={'first_goal_finished_3':'DOCKING_IDLE', 'robot_falls_0':'ROBOT_FAIL_1','repeat':'WALKING_IDLE_1'})
+        smach.StateMachine.add('S1', S1(), 
+                               transitions={'second':'S2', 'run_into_wall':'RUN_INTO_WALL','repeat':'S1'})
 
-        smach.StateMachine.add('DOCKING_IDLE', DOCKING_IDLE(), 
-                               transitions={'docking_finished_5':'IDLE_IDLE_2', 'robot_falls_0':'ROBOT_FAIL_1','repeat':'DOCKING_IDLE'})     
+        smach.StateMachine.add('S2', S2(), 
+                               transitions={'third':'S3', 'run_into_wall':'RUN_INTO_WALL','repeat':'S2'})     
 
-        smach.StateMachine.add('IDLE_IDLE_2', IDLE_IDLE_2(), 
-                               transitions={'moving_box_started_19':'IDLE_WORKING','repeat':'IDLE_IDLE_2'})     
+        smach.StateMachine.add('S3', S3(), 
+                               transitions={'forth':'S4', 'collision2':'COLLISION2','repeat':'S3'})     
 
-        smach.StateMachine.add('IDLE_WORKING', IDLE_WORKING(), 
-                               transitions={'moving_box_ended_7':'IDLE_IDLE_3', 'box_falls_2':'BOX_FAIL','repeat':'IDLE_WORKING'})     
+        smach.StateMachine.add('S4', S4(), 
+                               transitions={'fifth':'S5', 'collision2':'COLLISION2','repeat':'S4'})     
 
-        smach.StateMachine.add('IDLE_IDLE_3', IDLE_IDLE_3(), 
-                               transitions={'box_is_on_the_robot_21':'STANDING_IDLE','repeat':'IDLE_IDLE_3'})     
+        smach.StateMachine.add('S5', S5(), 
+                               transitions={'sixth':'S6', 'collision3':'COLLISION3','repeat':'S5'})     
 
-        smach.StateMachine.add('STANDING_IDLE', STANDING_IDLE(), 
-                               transitions={'second_goal_started_9':'WALKING_IDLE_2', 'robot_falls_4':'ROBOT_FAIL_2','repeat':'STANDING_IDLE'})     
+        smach.StateMachine.add('S6', S6(), 
+                               transitions={'seventh':'S7', 'collision3':'COLLISION3','repeat':'S6'})     
 
-        smach.StateMachine.add('WALKING_IDLE_2', WALKING_IDLE_2(), 
-                               transitions={'second_goal_finished_11':'SUCCES_IDLE', 'robot_falls_4':'ROBOT_FAIL_2','repeat':'WALKING_IDLE_2'})     
+        smach.StateMachine.add('S7', S7(), 
+                               transitions={'eighth':'S0', 'collision4':'COLLISION4','repeat':'S7'})     
 
-        smach.StateMachine.add('SUCCES_IDLE', SUCCES_IDLE(), 
-                               transitions={'succes_flag_13':'IDLE_IDLE_1'
+        smach.StateMachine.add('COLLISION4', FALL4(), 
+                               transitions={'reset4':'S7'
                                })
         #Codes for smach viewer
         sis = smach_ros.IntrospectionServer('rover_state_machine', sm_rover, '/ROVER_SM_ROOT')
--- status_handler.py
+++ status_handler.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 import rospy
-from aliengo_state_mach.msg import RoverActionMsg,RoverStateMsg
+from turtlebot3_msgs.msg import RoverActionMsg,RoverStateMsg
 from std_msgs.msg import String
 
 
 class status_handler:
-     def __init__(self):
+    def __init__(self):
  
-         self.action = RoverActionMsg()
-     def start(self):
-         self.state_pub = rospy.Publisher("/smach/output", RoverStateMsg, queue_size=1)
-         rospy.Subscriber("/smach/input",RoverActionMsg,self.state_callback)
-   
-     def state_callback(self,data):
-         self.action.ACTION = data.ACTION
+        self.action = RoverActionMsg()
 
-     def publishRoverState(self, state_msg):		
-         self.state_pub.publish(state_msg)
+    def start(self):
+    	self.state_pub = rospy.Publisher("/smach/output", RoverStateMsg, queue_size=1)
+        rospy.Subscriber("/smach/input",RoverActionMsg,self.state_callback)
+
+    def state_callback(self,data):
+    	self.action.ACTION = data.ACTION
+
+    def publishRoverState(self, state_msg):		
+        self.state_pub.publish(state_msg)
 
 
 
